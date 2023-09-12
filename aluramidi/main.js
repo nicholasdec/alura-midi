2f317f205b32b75af254333da0f62ca3764e9da0
nicholasdec <112396662+nicholasdec@users.noreply.github.com>
2023-09-12 09:36:42 -0300

    Add files via upload

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,19 +1,10 @@
+// Pom -------------------------------------
 function tocaSomPom () {
 document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
-    }
-    
-    document.querySelector('.tecla_clap').onclick = tocaSomClap;
+ const listaDeTeclas = document.querySelectorAll('.tecla');
 
-    function tocaSomTim () {
-        document.querySelector('#som_tecla_tim').play();
-        }
-        
-        document.querySelector('.tecla_tim').onclick = tocaSomTim;
-    
+listaDeTeclas[0].onclick =tocaSomPom;
