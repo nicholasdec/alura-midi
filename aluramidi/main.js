c28a52ad0cf74ef100fdb2ad172dc75d9ddbe9bc
nicholasdec <112396662+nicholasdec@users.noreply.github.com>
2023-10-03 09:38:58 -0300

    Add files via upload

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -8,22 +8,23 @@ document.querySelector(idElementoAudio).play();
 
 let contador = 0;
 
-//enquanto
-while (contador < listaDeTeclas.length) {
+//para
+for ( let contador = 0; contador < listaDeTeclas.length; contador++) {
 
 const tecla = listaDeTeclas[contador];    
 const instrumento = tecla.classList[1];
-//template string
-const idAudio ='#som_${instrumento}';
 
-//console.log(idAudio);
+const idAudio ='#som_${instrumento}'; //template string
+
 
 tecla.onclick = function () {
 tocaSom(idAudio);
 }
 
-contador = contador + 1;
-
-//console.log(contador);
+tecla.onkeydown = function() {
+    tecla.classList.add('ativa');
 }
+
+}
+
 
